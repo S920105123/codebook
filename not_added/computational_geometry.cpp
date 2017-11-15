08c01325231c7b5363adf17a49e5df43ba890e20
SheepRanger <milk500ted939@gmail.com>
2017-11-15 16:52:39 +0800

    Convex hull

--- not_added/computational_geometry.cpp
+++ not_added/computational_geometry.cpp
@@ -1,52 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
 const double PI=acos(-1);
-template<class T>
+
 struct Point {
 	// Also a vector
-	T x,y;
-	Point<T> operator+(const Point<T> &p) const {
+	double x,y;
+	Point operator+(const Point &p) const {
 		return {x+p.x,y+p.y};
 	}
-	Point<T> operator-(const Point<T> &p) const {
+	Point operator-(const Point &p) const {
 		return {x-p.x,y-p.y};
 	}
-	Point<T> operator*(const T &mul) const {
+	Point operator*(double mul) const {
 		return {x*mul,y*mul};
 	}
-	Point<T> operator/(const T &mul) const {
+	Point operator/(double mul) const {
 		return {x/mul,y/mul};
 	}
-	bool operator==(const Point<T> &p) const {
+	bool operator==(const Point &p) const {
 		return x==p.x&&y==p.y;
 	}
-	T cross(const Point<T> &v) const {
+	double cross(const Point &v) const {
 		return x*v.y-y*v.x;
 	}
-	T dot(const Point<T> &v) const {
+	double dot(const Point &v) const {
 		return x*v.x+y*v.y;
 	}
-	Point<T> normal() { // Normal vector
+	Point normal() { // Normal vector
 		return {-y,x};
 	}
-	T len() { // length
+	double len() const { // length
 		return sqrt(x*x+y*y);
 	}
-	T angle(const Point<T> &v) {
+	double angle(const Point &v) const {
 		// Angle from *this to v in [-pi,pi].
 		return atan2(cross(v),dot(v));
 	}
+	Point rotate_about(double theta, const Point &p) const {
+		// Rotate this point conterclockwise by theta about p
+		double nx=x-p.x,ny=y-p.y;
+		return {nx*cos(theta)-ny*sin(theta)+p.x,nx*sin(theta)+ny*cos(theta)+p.y};
+	}
 };
 
-template<class T>
 struct Line {
 	// Also a segment
-	Point<T> p1,p2;
-	T a,b,c; // ax+by+c=0
+	Point p1,p2;
+	double a,b,c; // ax+by+c=0
 	Line(){}
-	Line(const Point<T> &_p1, const Point<T> &_p2) {
+	Line(const Point &_p1, const Point &_p2) {
 		p1=_p1; p2=_p2;
-		a=p1.y-p2.y;
-		b=p2.x-p1.x;
-		c=-a*p1.x-b*p1.y;
+		pton();
 	}
     void pton() {
         // IMPORTANT if you don't use constructor.
@@ -54,45 +58,37 @@ struct Line {
 		b=p2.x-p1.x;
 		c=-a*p1.x-b*p1.y;
     }
-	int relation(const Point<T> &p) {
+	int relation(const Point &p) {
 		// For line, 0 if point on line
 		// -1 if left, 1 if right
-		Point<T> dir=p2-p1;
-		T crs=dir.cross(p-p1);
+		Point dir=p2-p1;
+		double crs=dir.cross(p-p1);
 		return crs==0?0:crs<0?-1:1;
 	}
-	Point<T> normal() {
-		Point<T> dir=p2-p1;
+	Point normal() {
+		Point dir=p2-p1;
 		return {-dir.y,dir.x};
 	}
-	bool on_segment(const Point<T> &p) { // Point on segment
+	bool on_segment(const Point &p) { // Point on segment
 		return relation(p)==0&&(p2-p).dot(p1-p)<=0;
 	}
-	bool parallel(const Line<T> &l) { // Two line parallel
+	bool parallel(const Line &l) { // Two line parallel
 		return (p2-p1).cross(l.p2-l.p1)==0;
 	}
-	bool equal(const Line<T> &l) { // Two line equal
+	bool equal(const Line &l) { // Two line equal
 		return relation(l.p1)==0&&relation(l.p2)==0;
 	}
-	bool cross_seg(const Line<T> &seg) { // Line intersect segment
-		Point<T> dir=p2-p1;
+	bool cross_seg(const Line &seg) { // Line intersect segment
+		Point dir=p2-p1;
 		return dir.cross(seg.p1-p1)*dir.cross(seg.p2-p1)<=0;
 	}
-	Point<T> line_intersection(const Line<T> &l) const{ // Intersection of lines
-		T deno=a*l.b-l.a*b;
-		if (deno!=0) {
-			return { (l.c*b-c*l.b)/deno, (l.a*c-a*l.c)/deno};
-		}
-		// Reaches here means no intersection. (parallel)
-        return {1234,4321};
-	}
-	int seg_intersect(const Line<T> &s) const{ // Two segment intersect
+	int seg_intersect(const Line &s) const{ // Two segment intersect
 		// 0 -> no, 1 -> one point, -1 -> infinity
-		Point<T> dir=p2-p1, dir2=s.p2-s.p1;
-		T c1=dir.cross(s.p2-p1);
-		T c2=dir.cross(s.p1-p1);
-		T c3=dir2.cross(p2-s.p1);
-		T c4=dir2.cross(p1-s.p1);
+		Point dir=p2-p1, dir2=s.p2-s.p1;
+		double c1=dir.cross(s.p2-p1);
+		double c2=dir.cross(s.p1-p1);
+		double c3=dir2.cross(p2-s.p1);
+		double c4=dir2.cross(p1-s.p1);
 		if (c1==0&&c2==0) {
 			if((s.p2-p1).dot(s.p1-p1)>0&&(s.p2-p2).dot(s.p1-p2)>0&&
 			   (p1-s.p1).dot(p2-s.p1)>0&&(p1-s.p2).dot(p2-s.p2)>0)return 0;
@@ -104,12 +100,22 @@ struct Line {
 		}else if(c1*c2<=0&&c3*c4<=0)return 1;
 		return 0;
 	}
-	Point<T> seg_intersection(Line<T> &s) const {
-		Point<T> dir=p2-p1, dir2=s.p2-s.p1;
-		T c1=dir.cross(s.p2-p1);
-		T c2=dir.cross(s.p1-p1);
-		T c3=dir2.cross(p2-s.p1);
-		T c4=dir2.cross(p1-s.p1);
+	Point line_intersection(const Line &l) const{ // Intersection of lines
+		// pton(); l.pton();
+		double deno=a*l.b-l.a*b;
+		if (deno!=0) {
+			return { (l.c*b-c*l.b)/deno, (l.a*c-a*l.c)/deno};
+		}
+		// Reaches here means no intersection. (parallel)
+        return {1234,4321};
+	}
+	Point seg_intersection(Line &s) const {
+		Point dir=p2-p1, dir2=s.p2-s.p1;
+		// pton(); l.pton();
+		double c1=dir.cross(s.p2-p1);
+		double c2=dir.cross(s.p1-p1);
+		double c3=dir2.cross(p2-s.p1);
+		double c4=dir2.cross(p1-s.p1);
 		if (c1==0&&c2==0) {
 			if(p1==s.p1&&(p2-p1).dot(s.p2-p1)<=0)return p1;
 			if(p1==s.p2&&(p2-p1).dot(s.p1-p1)<=0)return p1;
@@ -120,23 +126,22 @@ struct Line {
 		// Use seg_intersect to check OuO
         return {1234,4321};
 	}
-	T dist(const Point<T> &p, bool is_segment) const {
+	double dist(const Point &p, bool is_segment) const {
 		// Point to Line/segment
-		Point<T> dir=p2-p1,v=p-p1;
+		Point dir=p2-p1,v=p-p1;
 		if (is_segment) {
 			if (dir.dot(v)<0) return v.len();
 			if ((p1-p2).dot(p-p2)<0) return (p-p2).len();
 		}
-		T d=abs(dir.cross(v))/dir.len();
+		double d=abs(dir.cross(v))/dir.len();
 		return d;
 	}
 };
 
-template <class T>
 struct Polygon {
-	vector<Point<T>> V; // Counterclockwise
-	T area() const {
-		T res=0;
+	vector<Point> V; // Counterclockwise
+	double area() const {
+		double res=0;
 		for (int i=1;i+1<V.size();i++) {
 			res+=(V[i]-V[0]).cross(V[i+1]-V[0]);
 		}
@@ -145,8 +150,9 @@ struct Polygon {
 
 };
 
-template <class T>
 struct Circle {
-	T r; // Radius
-	Point<T> c; // Center
+	double r; // Radius
+	Point c; // Center
 };
+
+
